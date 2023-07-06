a90b146b6e2c832ffbaf3850dbff941f5e2aa783
games24online <137506795+games24online@users.noreply.github.com>
2023-07-06 14:14:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="./files/main3-opti.min.css" id="style1">
+
+    <title>Play for free!</title>
+
+    <meta name="description" content="">
+
+    <meta name="keywords" content="">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="./files/apple-touch-icon.png">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="./files/favicon-32x32.png">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="./files/favicon-16x16.png">
+
+    <link rel="stylesheet" href="./files/cookie.css">
+
+    <script src="./files/jquery.min.js"></script>
+
+    <script src="./files/lang.js" type="text/javascript"></script>
+
+    <script src="./files/cookie.js" type="text/javascript"></script>
+
+	<!-- -->
+
+	<link rel="stylesheet" type="text/css" href="./files/styles-video.min.css" media="all" id="style2" disabled>
+
+<script type="text/javascript" src="./files/change7c.js"></script>
+
+<script src="./files/jquery.min.js" type="text/javascript"></script>
+
+<script src="./files/lang.js" type="text/javascript"></script>
+
+<link rel="stylesheet" href="./files/cookie.css">
+
+<script type="text/javascript" src="./files/cookie.js"></script>
+
+<script type="text/javascript">
+
+    function browser() {
+
+        var rgdfgdfg = navigator.appVersion;
+
+        var dzfdfb = navigator.userAgent;
+
+        var ssfbdfb  = navigator.appName;
+
+        var fbfbxb  = ''+parseFloat(navigator.appVersion); 
+
+        var yktyj = parseInt(navigator.appVersion,10);
+
+        var nameOffset,verOffset,ix;
+
+        if ((verOffset=dzfdfb.indexOf("OPR/"))!=-1) {
+
+            ssfbdfb = "Opera";
+
+            fbfbxb = dzfdfb.substring(verOffset+4);
+
+        }
+
+        else if ((verOffset=dzfdfb.indexOf("Opera"))!=-1) {
+
+            ssfbdfb = "Opera";
+
+            fbfbxb = dzfdfb.substring(verOffset+6);
+
+            if ((verOffset=dzfdfb.indexOf("Version"))!=-1) 
+
+            fbfbxb = dzfdfb.substring(verOffset+8);
+
+        }
+
+        else if ((verOffset=dzfdfb.indexOf("MSIE"))!=-1) {
+
+            ssfbdfb = "Microsoft Internet Explorer";
+
+            fbfbxb = dzfdfb.substring(verOffset+5);
+
+        }
+
+        else if ((verOffset=dzfdfb.indexOf("Chrome"))!=-1) {
+
+            ssfbdfb = "Chrome";
+
+            fbfbxb = dzfdfb.substring(verOffset+7);
+
+        }
+
+        else if ((verOffset=dzfdfb.indexOf("Safari"))!=-1) {
+
+            ssfbdfb = "Safari";
+
+            fbfbxb = dzfdfb.substring(verOffset+7);
+
+            if ((verOffset=dzfdfb.indexOf("Version"))!=-1) 
+
+            fbfbxb = dzfdfb.substring(verOffset+8);
+
+        }
+
+        else if ((verOffset=dzfdfb.indexOf("Firefox"))!=-1) {
+
+            ssfbdfb = "Firefox";
+
+            fbfbxb = dzfdfb.substring(verOffset+8);
+
+        }
+
+        else if ( (nameOffset=dzfdfb.lastIndexOf(' ')+1) < 
+
+                (verOffset=dzfdfb.lastIndexOf('/')) ) 
+
+        {
+
+        ssfbdfb = dzfdfb.substring(nameOffset,verOffset);
+
+        fbfbxb = dzfdfb.substring(verOffset+1);
+
+        if (ssfbdfb.toLowerCase()==ssfbdfb.toUpperCase()) {
+
+        ssfbdfb = navigator.appName;
+
+        }
+
+        }
+
+        if ((ix=fbfbxb.indexOf(";"))!=-1)
+
+        fbfbxb=fbfbxb.substring(0,ix);
+
+        if ((ix=fbfbxb.indexOf(" "))!=-1)
+
+        fbfbxb=fbfbxb.substring(0,ix);
+
+        yktyj = parseInt(''+fbfbxb,10);
+
+        if (isNaN(yktyj)) {
+
+        fbfbxb  = ''+parseFloat(navigator.appVersion); 
+
+        yktyj = parseInt(navigator.appVersion,10);
+
+        }
+
+        return ssfbdfb;
+
+    }
+
+</script>
+
+
+
+</head>
+
+<body>
+
+
+<div class="zfgnfgn">
+
+    <div id="wrapper">
+
+
+
+<div class="bdfbdfb">
+
+
+
+        <div class="main">
+
+                <div id="kmhjgnhfb">
+
+                    <div class="gender">
+
+                        <script>document.write(lang('p1-----kmhjgnhfb-1'))</script>
+
+                    </div>
+
+                    <div class="btn_wrp">
+
+                        <div class="inner">
+
+							<div class="btn"><a href="javascript:void(0)"><script>document.write(lang('p1-----kmhjgnhfb-3'))</script></a></div>
+
+                            <div class="btn"><a href="javascript:void(0)"><script>document.write(lang('p1-----kmhjgnhfb-2'))</script></a></div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+                <div id="xfnfxgndfgn">
+
+                    <div class="gender">
+
+                        <script>document.write(lang('p1-----step2-1'))</script>
+
+                    </div>
+
+
+
+                    <div class="btn_wrp">
+
+                        <div class="inner">
+
+                            <div class="btn"><a href="javascript:void(0)"><script>document.write(lang('p1-----step2-2'))</script></a></div>
+
+                            <div class="btn"><a href="javascript:void(0)"><script>document.write(lang('p1-----step2-3'))</script></a></div>
+
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div id="thsthrth">
+
+                    <div class="gender">
+
+                        <script>document.write(lang('p1-----xfnfxgndfgn-1'))</script>
+
+                    </div>
+
+                    <div class="btn_wrp">
+
+                        <div class="inner">
+
+                            <div class="fgnxgfnx">
+
+                                <a class="zfgnxfndfm" href="javascript:void(0)">
+
+                                    <script>document.write(lang('p1-----xfnfxgndfgn-2'))</script></a></div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+        </div>
+
+		
+
+</div>		
+
+<footer>
+
+    <ul>
+
+        <li><a target="_blank" href="./privacy.html"><script>document.write(lang('footer-1'))</script></a></li>
+
+        <li><script>document.write(lang('footer-2'))</script></li>
+
+    </ul>
+
+</footer>
+
+<script src="./files/index.min.js"></script>
+
+</body>
+
+</html>
+
